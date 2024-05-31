d2bfe67cbb86d113dc23b40ce95472f0fc8b0cbb
R S Lora <loremoon258@gmail.com>
2024-05-31 16:56:37 +0530

    add

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,18 @@
 import Header from "./Layout/Header";
 import AccordionPage from "./Pages/AccordionPage";
 import Founder from "./Pages/Founder";
+import Welcome from "./Pages/Welcome";
+import School from "./Pages/School";
 
 function App() {
   return (
     <>
       <Header />
       <Founder />
+      <Welcome/>
+      <School/>
       <AccordionPage />
+   
     </>
   );
 }
